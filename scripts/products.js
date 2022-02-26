0f0d30ea20407086c08b038843e7d7a5f7391d4e
Anjum Ara <anjumara0711@gmail.com>
2022-02-26 23:06:47 +0530

    search ,sort and filter

--- scripts/products.js
+++ scripts/products.js
@@ -13,10 +13,71 @@ async function initial() {
   display(products);
 }
 
+document.querySelector("#sortPrice").addEventListener("change",sortByPrice)
+
+async function sortByPrice()
+{
+  let selected = document.querySelector("#sortPrice").value;
+  let res = await fetch(
+    `https://zara-server.herokuapp.com/products?section=${query}`
+  );
+  let products = await res.json();
+  if(selected == "low")
+  {
+    products.sort((a,b) => {
+      return a.price - b.price
+    })
+  }
+  if(selected == "high")
+  {
+    products.sort((a,b) => {
+      return b.price - a.price
+    }) 
+  }
+  if(selected == "less"){
+    products = products.filter((product) => {
+      console.log("here")
+      return product.price <= 2000;
+      
+    });
+  }
+  if(selected == "greater"){
+    products = products.filter((product) => {
+      return product.price >= 500;
+      
+    });
+  }
+  console.log("Products : ", products);
+  display(products);
+}
+
+// async function sortByPrice()
+// {
+//     var selected = document.querySelector("#sortPrice").value;
+//     if(selected == "low")
+//     {
+//       let res = await fetch(
+//         `https://zara-server.herokuapp.com/products/lowToHigh`
+//       );
+    
+//       var products = await res.json();
+//       console.log(products)
+//     }
+//     if(selected == "high")
+//     {
+//       let res = await fetch(
+//         `https://zara-server.herokuapp.com/products/highToLow`
+//       );
+    
+//       var products = await res.json(); 
+//       console.log(products)
+//     }
+//     // display(products);
+// }
 
 function display(data) {
   let main = document.getElementById("main");
-
+  main.innerHTML = "";
   data.forEach(function (elem) {
     let div = document.createElement("div");
 
--- scripts/search.js
+++ scripts/search.js
@@ -9,13 +9,11 @@ async function load() {
 
   let products = await res.json();
 
-  console.log("Products : ", products);
+  // console.log("Products : ", products);
 
   let mensShirt = products.filter((data) => data.section === "mensShirt");
   let mensShoes = products.filter((data) => data.section === "mensShoes");
-  let womenAccessories = products.filter(
-    (data) => data.section === "womenAccessories"
-  );
+  let womenAccessories = products.filter((data) => data.section === "womenAccessories");
   let womenJacket = products.filter((data) => data.section === "womenJacket");
 
   showshirts(mensShirt);
@@ -25,9 +23,58 @@ async function load() {
 
   
 }
+let input = document.getElementById("searchbar");
+input.addEventListener('keypress',search);
+
+async function search(event){
+    let searchProduct = input.value;
+    if(searchProduct == "men shirts" || searchProduct == "men shirt" || searchProduct == "shirts" || searchProduct == "shirt"){
+      searchProduct = "mensShirt"
+    }
+    else if(searchProduct == "men shoes" || searchProduct == "shoes"){
+      searchProduct = "mensShoes"
+    }
+    else if(searchProduct == "women accessories" || searchProduct == "accessories"){
+      searchProduct = "womenAccessories"
+    }
+    else if(searchProduct == "women jackets" || searchProduct == "women jacket" || searchProduct == "jackets" || searchProduct == "jacket"){
+      searchProduct = "womenJacket"
+    }
+    let res = await fetch(`https://zara-server.herokuapp.com/products?section=${searchProduct}`);
+    let products = await res.json();
+    if(event.key == 'Enter'){
+      // console.log(products)
+      showProduct(products)
+    }
+}
+let proddiv1 = document.querySelector("#searchProd");
+
+function showProduct(elem) {
+  document.querySelector("#shirt-products").classList.add("none");
+  document.querySelector("#shoes-products").classList.add("none");
+  document.querySelector("#womenAccessories-products").classList.add("none");
+  document.querySelector("#women-jacket").classList.add("none");
+  document.querySelector("#searchProd").classList.remove("none");
+  proddiv1.innerHTML = ""
+  elem.map(function (data) {
+    contdiv = document.createElement("div");
+    let img = document.createElement("img");
+    let div = document.createElement("div");
+    let p1 = document.createElement("p");
+    let p2 = document.createElement("p");
+
+    img.src = data.imgUrl;
+    p1.innerText = data.prod_name;
+    p2.innerText = data.price;
+
+    div.append(p1, p2);
+    contdiv.append(img, div);
+    proddiv1.append(contdiv)
+  });
+
+}
 
 function show(elem) {
-  
  let prod =  elem.map(function (data) {
     contdiv = document.createElement("div");
     let img = document.createElement("img");
@@ -52,7 +99,7 @@ function show(elem) {
 
 // show Women jacket function
 function showwomenjacket(elem) {
-  console.log("womenjacket:", elem);
+  // console.log("womenjacket:", elem);
   let proddiv = document.querySelector("#women-jacket");
   let prod = show(elem);
   prod.forEach((data) => proddiv.append(data));
@@ -61,7 +108,7 @@ function showwomenjacket(elem) {
 //show Women Accessories function
 
 function showwomenAccessories(elem) {
-  console.log("womenAccessories:", elem);
+  // console.log("womenAccessories:", elem);
   let proddiv = document.querySelector("#womenAccessories-products");
  let prod = show(elem);
  prod.forEach((data) => proddiv.append(data));
@@ -70,7 +117,7 @@ function showwomenAccessories(elem) {
 //show shoes function
 
 function showshoes(elem) {
-  console.log("shoes:", elem);
+  // console.log("shoes:", elem);
   let proddiv = document.querySelector("#shoes-products");
  let prod = show(elem);
  prod.forEach((data) => proddiv.append(data));
@@ -90,10 +137,11 @@ function showshirts(elem) {
 function shirts(event) {
   event.preventDefault();
 
-  console.log(event.target);
+  // console.log(event.target);
 
   document.querySelector("#shirt-products").classList.toggle("none");
   document.querySelector("#shoes-products").classList.add("none");
+  document.querySelector("#searchProd").classList.add("none");
   document.querySelector("#womenAccessories-products").classList.add("none");
   document.querySelector("#women-jacket").classList.add("none");
 }
@@ -103,6 +151,7 @@ function womenjacket(event) {
   event.preventDefault();
 
   document.querySelector("#women-jacket").classList.toggle("none");
+  document.querySelector("#searchProd").classList.add("none");
   document.querySelector("#womenAccessories-products").classList.add("none");
   document.querySelector("#shoes-products").classList.add("none");
   document.querySelector("#shirt-products").classList.add("none");
@@ -115,6 +164,7 @@ function womenAcc(event) {
   document.querySelector("#womenAccessories-products").classList.toggle("none");
   document.querySelector("#shoes-products").classList.add("none");
   document.querySelector("#shirt-products").classList.add("none");
+  document.querySelector("#searchProd").classList.add("none");
   document.querySelector("#women-jacket").classList.add("none");
 }
 
@@ -122,6 +172,7 @@ function womenAcc(event) {
 function shoes(event) {
   event.preventDefault();
 
+  document.querySelector("#searchProd").classList.add("none");
   document.querySelector("#shoes-products").classList.toggle("none");
   document.querySelector("#shirt-products").classList.add("none");
   document.querySelector("#womenAccessories-products").classList.add("none");
