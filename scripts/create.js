f148b82bcbcde5906c5ea894b3fa1186621728ef
bablikumari2 <anandbabli96@gmail.com>
2022-02-26 15:23:40 +0530

    login-sigup

---
+++ scripts/create.js
@@ -0,0 +1,50 @@
+let token = JSON.parse(localStorage.getItem("token"));
+
+if (token) {
+  alert("You are already logged in");
+  window.location.href = "./index.html";
+
+}
+
+function signUp(event) {
+  event.preventDefault();
+  var form = event.target;
+  var data = {
+    name: form.name.value,
+    email: form.email.value,
+    password: form.password.value,
+    repeatPassword: form.reapeatpassword.value,
+    address: form.address.value,
+    city: form.city.value,
+    state: form.state.value,
+    pincode: form.pin.value,
+    morenfo: form.optional.value,
+    locality: form.locality.value,
+    phone: form.tel.value,
+  };
+
+  console.log(data);
+
+  register(JSON.stringify(data));
+}
+
+async function register(data) {
+  let res = await fetch("https://zara-server.herokuapp.com/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: data,
+  });
+
+  let response = await res.json();
+
+  if (response.message) {
+    alert(response.message);
+    return;
+  }
+
+  alert("Registered Successfully");
+
+  window.location.href = "login.html";
+}
---
+++ scripts/login.js
@@ -0,0 +1,62 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+
+
+function clickForPass() {
+  var height = (document.querySelector("#inputEmail").style.height = "block");
+  var passwd = (document.querySelector("#inputPasswd").style.display = "block");
+}
+
+
+function login(event) {
+  event.preventDefault();
+
+  let form = event.target;
+
+   let data = {
+     email: form.inputEmail.value,
+     password: form.inputPasswd.value,
+   };
+
+   console.log(data)
+
+   // sending data to server in json format
+   loginUser(JSON.stringify(data));
+
+ 
+}
+
+async function loginUser(data) {
+  let res = await fetch("https://zara-server.herokuapp.com/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: data,
+  });
+
+  let response = await res.json();
+
+  if(response.message){
+    alert(response.message);
+    return
+  }
+
+  alert("Login Successful");
+
+  let name = response.user.name;
+
+  localStorage.setItem("userName", JSON.stringify(name));
+  localStorage.setItem("token", JSON.stringify(response.token));
+
+  if(cart.length){
+    window.location.href = "shipping.html";
+    return
+  }
+
+  window.location.href = "index.html";
+
+  console.log(name);
+
+}
+
